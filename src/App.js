f33988ca80949a2b4a12b1bdeced8972b1766a63
Ravuri Varshitha <varshitharamesh.ravuri@gmail.com>
2025-09-24 12:37:38 +0530

    Added basic negotion logic in javascript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){const intialPrice = 100;
+  function negotiate(offer){
+    if(offer<initialPrice *0.8){
+      return 'Too low!Try $${initialPrice *0.9}.';
+
+    }
+    return "Deal accepted!";
+  }
+  return(
+    <div>
+      <h1>EduMarket</h1>
+      <p>{negotiate(80)}</p>
     </div>
   );
-}
 
+}
 export default App;
